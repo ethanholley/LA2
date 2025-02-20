0e7741431fe88b75f58ac667d9e4db746a4957f3
Chance Krueger <chancekrueger@arizona.edu>
2025-02-20 09:28:34 -0700

    This is a test

--- src/model/Album.java
+++ src/model/Album.java
@@ -5,6 +5,20 @@ import java.util.ArrayList;
 public class Album {
 	private String artist;
 	private ArrayList<Song> songList = new ArrayList<Song>();
-	
+
+	public Album(String artist, ArrayList<Song> songList) {
+		this.artist = artist;
+		this.songList = songList;
+	}
+
+	// test
+	public String getArtist() {
+		return artist;
+	}
+
+	public ArrayList<Song> getSongList() {
+		return songList;
+	}
+
 	// hello
 }
